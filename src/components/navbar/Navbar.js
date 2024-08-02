7e189a03da48580a7596ab72ef067d60e70440fc
obiwan98 <funkholics@gmail.com>
2024-08-02 13:08:49 +0900

    네비게이션 바 내용 수정, 컬러 수정

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,20 +1,52 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import './Navbar.css';
 
 function Navbar({ isLoggedIn, setIsLoggedIn }) {
+  const [user, setUser] = useState(null);
   const history = useHistory();
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const token = localStorage.getItem('token');
+        if (!token) return;
+
+        const response = await axios.get(process.env.REACT_APP_API_URL + '/api/users/me', {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        setUser(response.data);
+      } catch (error) {
+        console.error('Error fetching user data:', error);
+        localStorage.removeItem('token');
+        setUser(null);
+      }
+    };
+
+    fetchUser();
+  }, [isLoggedIn]);
 
   const handleLogout = () => {
     localStorage.removeItem('token');
     setIsLoggedIn(false);
+    setUser(null);
     history.push('/login');
   };
 
   return (
     <div className="navbar">
       {isLoggedIn ? (
+        <>
+        {user && (
+            <div className="user-info">
+              {user.email} [{user.role.roleName}, {user.group.team}]
+            </div>
+          )}
         <button onClick={handleLogout} className="logout-button">로그아웃</button>
+        </>
       ) : (
         <>
           <Link to="/signup">회원가입</Link>
