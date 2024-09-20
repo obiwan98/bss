4080ebaa2951b16c34d6eb80211000c5fac031b3
haejin <haejin891212@gmail.com>
2024-09-20 10:29:01 +0900

    도서 조회 화면 컴포넌트 구조 변경

--- src/components/content/management/BookModal.js
+++ src/components/content/management/BookModal.js
@@ -1,11 +1,14 @@
 import { Modal } from 'antd';
 
 import BookAdd from './BookAdd';
+import BookDetailView from './BookDetailView';
 
 const BookModal = ({ bookData, open, onCancel }) => {
+  const ModalComponent = !bookData ? BookAdd : BookDetailView;
+
   return (
     <Modal width={800} open={open} footer={null} onCancel={() => onCancel(false)}>
-      <BookAdd bookData={bookData} onCancel={onCancel} />
+      <ModalComponent bookData={bookData} onCancel={onCancel} />
     </Modal>
   );
 };
