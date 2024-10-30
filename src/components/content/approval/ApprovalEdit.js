627aceea21340080db9215e7b91a0b1d4824671d
EINE_CSB <seulbeom.choi@cj.net>
2024-10-30 16:51:51 +0900

    241030 통합 테스트 - 메일 송/수신 관련 로직 수정

--- src/components/content/approval/ApprovalEdit.js
+++ src/components/content/approval/ApprovalEdit.js
@@ -118,7 +118,17 @@ const ApprovalEdit = () => {
 
     setStateData((prevState) => ({
       ...prevState,
-      approvalTitle: isNew ? '승인 요청' : badgeState === 1 ? '승인 요청 상세':badgeState === 2 ? '승인 완료 상세':badgeState === 3 ? '반려 상세':badgeState === 4 ? '구매 완료 상세':'',
+      approvalTitle: isNew
+        ? '승인 요청'
+        : badgeState === 1
+          ? '승인 요청 상세'
+          : badgeState === 2
+            ? '승인 완료 상세'
+            : badgeState === 3
+              ? '반려 상세'
+              : badgeState === 4
+                ? '구매 완료 상세'
+                : '',
       approvalId: isNew ? rndUniqueId : state?._id,
 
       // 신청 정보
@@ -633,10 +643,11 @@ const ApprovalEdit = () => {
             };
 
             // 수신
-            // * 수신인 범위 지정 필요
             let recipient = {
-              to: 'seulbeom.choi@cj.net',
-              subject: 'SEND TEST',
+              to: Array.isArray(user?.leader?.email)
+                ? user.leader.email.join(', ')
+                : user?.leader?.email || 'N/A',
+              subject: '결재 요청 메일 안내',
             };
 
             // 도서 정보
@@ -647,11 +658,10 @@ const ApprovalEdit = () => {
             };
 
             // 결재 정보
-            // * 승인자의 범위 지정 필요
             let approvalInfo = {
-              approverName: 'TESTER',
+              approverName: user?.leader?.name || 'N/A',
               approvalDetails: confirmComment,
-              status: '승인',
+              status: badgeValue === 1 ? '승인' : badgeValue === 3 ? '반려' : '',
               date: formatDate(new Date()),
             };
 
@@ -766,8 +776,10 @@ const ApprovalEdit = () => {
     <div>
       <div style={{ display: 'flex', justifyContent: 'space-between', alignitems: 'center' }}>
         <h1>{approvalTitle}</h1>
-        <Button type="link" style={{ marginRight: '15px' }} 
-        onClick={() => navigate('/approval/list')}
+        <Button
+          type="link"
+          style={{ marginRight: '15px' }}
+          onClick={() => navigate('/approval/list')}
         >
           목록으로
         </Button>
--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,4 +1,5 @@
 import React, { createContext, useContext, useEffect, useState } from 'react';
+import axios from 'axios';
 const UserContext = createContext(null);
 
 // AuthProvider 컴포넌트
@@ -9,19 +10,38 @@ export const UserProvider = ({ children }) => {
     return savedUser ? JSON.parse(savedUser) : null;
   });
 
+  // 초기 사용자 그룹의 리더 구하기
+  const [leader, setLeader] = useState(null);
+  const getLeader = async () => {
+    try {
+      const response = await axios.get(
+        process.env.REACT_APP_API_URL +
+          `/api/users/group/${user?.group?._id}/role/66a0bbfe8d7e45a08668b30f/info`
+      );
+      setLeader(response.data.leaders[1]);
+    } catch (error) {
+      console.error('Error fetching dataList:', error);
+    }
+  };
+
   useEffect(() => {
     // user 상태가 변경될 때마다 localStorage에 저장
     if (user) {
-        localStorage.setItem('user', JSON.stringify(user));
+      localStorage.setItem('user', JSON.stringify(user));
+      getLeader();
     } else {
-        localStorage.removeItem('user');
+      localStorage.removeItem('user');
+      setLeader(null);
     }
   }, [user]);
 
+  const userLeader = {
+    ...user,
+    leader: leader,
+  };
+
   return (
-    <UserContext.Provider value={{ user, setUser }}>
-      {children}
-    </UserContext.Provider>
+    <UserContext.Provider value={{ user: userLeader, setUser }}>{children}</UserContext.Provider>
   );
 };
 
